d811375daf80a2b496cb3c8640f26e247bb2c983
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2024-03-08 16:43:58 -0400

    commented index.js/routes file

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,10 +1,10 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
+const router = require('express').Router(); //! this line is importing the express router
+const apiRoutes = require('./api'); //! this line is importing the api routes from the api folder
 
-router.use('/api', apiRoutes);
+router.use('/api', apiRoutes); //! this line is saying that the router will use the api routes
 
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+router.use((req, res) => { //! this line is saying that if the route is not found, it will send a 404 error
+  res.send("<h1>Wrong Route!</h1>") 
 });
 
-module.exports = router;
+module.exports = router; //! this line is exporting the router for use in other files
