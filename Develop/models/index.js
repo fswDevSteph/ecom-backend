01bd525da80c899a678a1cba13620c0cee68c7bb
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2024-03-08 16:40:26 -0400

    commented index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,19 +5,19 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: "category_id",
-  onDelete: "CASCADE"
+Product.belongsTo(Category, { //! this line is saying that the Product belongs to the Category
+  foreignKey: "category_id", //! this line is saying that the foreign key is category_id
+  onDelete: "CASCADE" //! this line is saying that if the category is deleted, the product will also be deleted
 })
 // Categories have many Products
-Category.hasMany(Product, {
-  foreignKey: "category_id",
+Category.hasMany(Product, { //! this line is saying that the Category has many Products
+  foreignKey: "category_id", //! this line is saying that the foreign key is category_id
   onDelete: "CASCADE"
 })
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {
-  foreignKey: "product_id",
-  through: ProductTag
+Product.belongsToMany(Tag, { //! this line is saying that the Product belongs to many Tags
+  foreignKey: "product_id", //! this line is saying that the foreign key is product_id
+  through: ProductTag  //! this line is saying that the through table is ProductTag. The Throughtable is a table that is created to hold the foreign keys of the two tables that are being connected
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
@@ -26,8 +26,8 @@ Tag.belongsToMany(Product, {
 })
 
 module.exports = {
-  Product,
-  Category,
+  Product, //! this line is exporting the Product model
+  Category, 
   Tag,
   ProductTag,
 };
