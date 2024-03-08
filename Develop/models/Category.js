c9ec9a4d9e5a0e2c7370c8808b6457474d9ccdb6
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2024-03-08 16:37:14 -0400

    comment category model for reference later if I forget

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,29 +1,29 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize'); //! this line is saying that we are importing the Model and DataTypes from the sequelize library
 
-const sequelize = require('../config/connection.js');
+const sequelize = require('../config/connection.js'); //! is importing the connection to the database from the connection.js file
 
-class Category extends Model {}
+class Category extends Model {} //! this line is creating the Category model by extending the Sequelize Model class
 
-Category.init(
+Category.init( //! this line is initializing the model's data and configuration, passing two objects as arguments
   {
     // define columns
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
+    id: { //* id column
+      type: DataTypes.INTEGER, //! data type is INTEGER
+      allowNull: false, //! it cannot be null
+      primaryKey: true, //! this is teh primary key
+      autoIncrement: true //! the id will auto increment
     },
-    category_name: {
-      type: DataTypes.STRING,
-      allowNull: false
+    category_name: { //* category_name column
+      type: DataTypes.STRING, //! data type is STRING
+      allowNull: false //! Cant be null
     }
   },
   {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
+    sequelize, 
+    timestamps: false, //! this line is saying that we don't want timestamps
+    freezeTableName: true, //! this line is saying that the table name won't be pluralized
+    underscored: true, //! this line is saying that the column names will be snake_case
+    modelName: 'category', //! this line is saying that the model name will be the lowercase version of the modelb name
   }
 );
 
