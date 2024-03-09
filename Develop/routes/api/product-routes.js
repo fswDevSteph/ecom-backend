f327c143647c5454b58850f146c5d7a27f5b7c4b
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2024-03-09 14:44:08 -0400

    get all products with category and tag data

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -3,9 +3,18 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
-router.get('/', (req, res) => {
+// get all products with category and tag data
+router.get('/', async (req, res) => {
   // find all products
+   try {
+    const products = await Product.findAll({
+      include: [{ model: Category }, { model: Tag, through: ProductTag }],
+    });
+    res.json(products);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Failed to retrieve products' });
+  }
   // be sure to include its associated Category and Tag data
 });
 
